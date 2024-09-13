609fea083965c9f63c2c9ad95b08e16857e28009
Nguyen Dinh Trung <nguyendinhtrung@syp.vn>
2024-09-13 12:35:32 +0700

    commit 139 noon

---
+++ flutter/training_app/lib/gradient_container.dart
@@ -0,0 +1,25 @@
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        // Đặt Text ở giữa
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
--- flutter/training_app/lib/main.dart
+++ flutter/training_app/lib/main.dart
@@ -1,11 +1,12 @@
 // Nhập thư viện Material Desiogn để sử dụng các widget có sẵn
 import 'package:flutter/material.dart';
+import 'package:training_app/gradient_container.dart';
 
 // Main Function, điểm bắt đầu của ứng dụng, khi ứng dụng được chạy sẽ gọi main đầu tiên
 void main() {
   runApp(
     // Widget gốc của ứng dụng
-    MaterialApp(
+    const MaterialApp(
       // Scaffold cung cấp một cấu trúc cơ bản cho ứng dụng, bao gồm thanh app bar, menu, và khu vực nội dung.
       home: Scaffold(
         // là 1 widget đa năng có thể chứa các widget con
@@ -13,29 +14,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        // Đặt Text ở giữa
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
-      ),
-    );
-  }
 }

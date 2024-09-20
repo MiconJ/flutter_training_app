20fe5cae64deae858699fd7a2a4bdab1307ac3cb
dinhtrung.6546@outlook.com.vn <trungdinh9911.>
2024-09-20 08:15:03 +0700

    209

--- flutter/quizz_app/lib/main.dart
+++ flutter/quizz_app/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quizz_app/start_screen.dart';
+import 'package:quizz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 12, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft, // Thêm điểm bắt đầu của gradient
-              end: Alignment.bottomRight, // Thêm điểm kết thúc của gradient
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
